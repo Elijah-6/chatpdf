c837490cdd0be80715f6c2508c848daa2750c562
Elijah-6 <elijahfrimpong76@gmail.com>
2023-09-28 17:24:31 +0000

    background changed

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,6 +2,8 @@ import Image from 'next/image'
 
 export default function Home() {
   return (
-    <h1 className='text-red-500'> Hello World</h1>
+    <div className='bg-gradient-to-r from-gray-200 via-gray-400 to-gray-600 h-screen w-screen'>
+      <h1 className='text-red-500'> Hello World</h1>
+    </div>
   )
 }
