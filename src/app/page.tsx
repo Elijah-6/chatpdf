e3424bb662c09b4102cab495d25f022e58a0431a
Elijah-6 <elijahfrimpong76@gmail.com>
2023-10-07 20:14:02 +0000

    clerk

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,15 @@
+import { UserButton } from '@clerk/nextjs'
 import Image from 'next/image'
 
 export default function Home() {
   return (
     <div className='bg-gradient-to-r from-gray-200 via-gray-400 to-gray-600 h-screen w-screen'>
-      <h1 className='text-red-500'> Hello World</h1>
+      <div className="absolute top-1/2 left-1/2">
+        <div className="flex-col">
+          <h1 className='text-red-50'> Hello World</h1>
+          <UserButton/>
+        </div>
+      </div>
     </div>
   )
 }
