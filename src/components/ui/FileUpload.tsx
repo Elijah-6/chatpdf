cbf727af5c8efd4a5484341049849772ac7a5f30
Elijah-6 <elijahfrimpong76@gmail.com>
2023-10-12 15:35:19 +0000

    file upload component

---
+++ src/components/ui/FileUpload.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import {useDropzone} from 'react-dropzone'
+
+type Props = {}
+
+const fileUpload = (props: Props) => {
+  return (
+    <div>fileUpload</div>
+  )
+}
+
+export default fileUpload
